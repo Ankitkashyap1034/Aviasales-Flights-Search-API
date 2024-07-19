772f5e53ef41049d330b922f875d95fc1c363c3d
Ankitkashyap1034 <ankitkumar103466@gmail.com>
2024-07-19 13:57:51 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+use Ankit04\AviasalesFlight\AviasalesFlight;
+
+$partnerId = '558305'; // marker
+$apiToekn = '4660a93d4860614d6f4eae4af7a8f0fa';
+$host = 'onelinktravel.com';
+
+
+$user_ip = $_SERVER['REMOTE_ADDR'];
+$user_ip = '103.182.161.48';
+$locale = 'en';
+$trip_class = 'Y';   // Y => 'econemy' , C  => 'Business. Required uppercase'
+$adult = 1;
+$children = 0;
+$infants = 0;
+
+$departure_date = "2024-08-21";
+$arrival_date = "2024-08-28";
+$origin = 'BOM';
+$destination = 'YYC';
+
+$passengers = [
+                'adults'=> $adult,
+                'children'=> $children,
+                'infants'=> $infants,
+                ];
+$segments = [
+                [
+                    'origin' => $origin,
+                    'destination' => $destination,
+                    'date' => $departure_date,
+                ],
+                [
+                    'origin' => $destination,
+                    'destination' => $origin,
+                    'date' => $arrival_date,
+                ]
+            ]; 
+$data = [
+    'user_ip' => $user_ip,
+    'locale' => $locale,
+    'trip_class' => $trip_class,
+    'adult' => $adult,
+    'children' => $children,
+    'infants' => $infants,
+    'departure_date' => $departure_date,
+    'arrival_date' => $arrival_date,
+    'origin' => $origin,
+    'destination' => $destination,
+];
+AviasalesFlight::initiate($data);
+
+
+// $plain_signature = $apiToekn.':'.$host.':'.$locale.':'.$partnerId.':'.$adult.':'.$children.':'.$infants.':'.$departure_date.':'.$destination.':'.$origin.':'.$arrival_date.':'.$destination.':'.$origin.':'.$trip_class.':'.$user_ip;
+// echo $plain_signature.'<br>';
+// echo md5($plain_signature);
